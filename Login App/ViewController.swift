a8a322008a3d714663af182e42eb4f755da5bd29
Ayomikun Adegoke <ayomikunadegoke@gmail.com>
2019-10-08 20:38:36 +0100

    Initial Commit

---
+++ Login App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Login App
+//
+//  Created by Adegoke Ayomikun on 08/10/2019.
+//  Copyright © 2019 Dev mikzy. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
